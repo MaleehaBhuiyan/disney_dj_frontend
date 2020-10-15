55be10af9633cb910b7f171f287321e00c97524f
MaleehaBhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-10-15 12:25:08 -0400

    Welcome page slide show with arrows done

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, withRouter } from 'react-router-dom';
 import './App.css';
-
-import Navbar from './Navbar'
 import Welcome from './Welcome'
 
 class App extends React.Component {
   render(){
     return (
       <>
-        <Navbar />
-        <Switch>
-        <Route path="/welcome" render={() => <Welcome />} />
-        </Switch>
+      <Welcome />
       </>
     )
   }
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,32 +4,8 @@ import { NavLink } from 'react-router-dom';
 class Navbar extends React.Component {
   render(){
     return (
-        <div>
-        <nav id="navbar" class="">
-        <div class="nav-wrapper">
-        
-            <div class="logo">
-
-            <a href="#home"><i class="fa fa-angellist"></i></a>
-            </div>
-
-        
-            <ul id="menu">
-            <li><NavLink to="/welcome" exact>Home</NavLink></li>
-  
-            {this.props.user ? 
-              <li><NavLink to="/welcome">
-                <li onClick={this.props.clickHandler}>Log Out</li>
-              </NavLink></li>
-              :
-              <li><NavLink to="/login">
-                <li>Login</li>
-              </NavLink></li>
-            }
-            </ul>
-        </div>
-        </nav>
-    </div>
+       <>
+       </>
     )
   }
 }
